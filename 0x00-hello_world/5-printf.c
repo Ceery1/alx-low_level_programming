771c802002ce75af3fb22b2cfa5c098c2e40bba1
mehdi belmoumen <mehdi.belmoumen@gmail.com>
2023-03-16 21:30:22 +0000

    use printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - write correct code
+ * Description: use printf to output the message
+ * Return: 0 is correct
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
