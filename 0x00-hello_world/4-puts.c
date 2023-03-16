b2aa2df005700cda1b19ea7f5efa6287e09cafa1
mehdi belmoumen <mehdi.belmoumen@gmail.com>
2023-03-16 21:10:32 +0000

    first code c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - My first C code using func puts
+* return: 0 is correct
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
