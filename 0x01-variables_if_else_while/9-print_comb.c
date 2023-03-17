903daa3b0a567d31282f3bc93f42829fea62ac1c
mehdi belmoumen <mehdi.belmoumen@gmail.com>
2023-03-17 21:36:28 +0000

    print single digit following by coma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print single digit and coma and space
+ * Return: 0 correct
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
