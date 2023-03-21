75956027ef03524dae61cc7b8987b4a48719f3bb
mehdi belmoumen <mehdi.belmoumen@gmail.com>
2023-03-21 22:02:58 +0000

    print _putchar

--- 0x01-variables_if_else_while/test.c
+++ 0x01-variables_if_else_while/test.c
@@ -3,11 +3,17 @@
 
 int main(void)
 {
-	int i;
-	
-	i = 1;
+	int i, y;
+
+	for (i = 0; i <= 20; i++)
+	{
+	for (y = i + 1; y <= 20; y++)
+	{
 	putchar(i % 10 + '0');
-	putchar(i / 10 + '0');
+	putchar(y % 10 + '0');
+	putchar(' ');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
