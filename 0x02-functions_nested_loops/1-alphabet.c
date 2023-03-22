42025e70c9e35996f1c3a25eb802a9faa4d452a5
mehdi belmoumen <mehdi.belmoumen@gmail.com>
2023-03-22 20:12:34 +0000

    print alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: print alphabet
+ * Return: 0 is correct
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
