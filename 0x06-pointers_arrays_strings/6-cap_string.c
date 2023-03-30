64b4e6022ea3670542a71de0172eef511add2932
Ceery1 <mehdi.belmoumen@gmail.com>
2023-03-30 23:22:12 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,15 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
-		str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
-		str[i] == '"' || str[i] == '(' || str[i] == ')' ||
-		str[i] == '{' || str[i] == '}')
+		if (str[i] == '\t')
+			str[i] = ' ';
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' ||
+		str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
+		str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+		str[i - 1] == '{' || str[i - 1] == '}')
 		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-				str[i + 1] = str[i + 1] - 32;
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = str[i] - 32;
 		}
 	}
 	return (str);
