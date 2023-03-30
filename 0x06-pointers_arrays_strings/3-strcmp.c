7461bc3dde5d25a8a9b71c293540a1ec6132fb24
Ceery1 <mehdi.belmoumen@gmail.com>
2023-03-30 20:27:10 +0000

    comare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,18 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int lengs1 = 0;
-	int lengs2 = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		lengs1++;
-	for (i = 0; s2[i] != '\0'; i++)
-		lengs2++;
-	if (lengs1 < lengs2)
-		return (-15);
-	else if (lengs1 > lengs2)
-		return (15);
-	else
-		return (0);
-
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		}
+		else
+			return (0);
+	}
+	return (0);
 }
