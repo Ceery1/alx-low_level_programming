0673de092dd76b8c969e1169242c63a92c2a7688
Ceery1 <mehdi.belmoumen@gmail.com>
2023-03-30 04:49:30 -0700

    concatenates

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - function to call
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lendest = 0;
+	int lensrc = 0;
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		lendest++;
+	}
+	for (i = 0; i != '\0'; i++)
+	{
+		lensrc++;
+	}
+	for (i = 0; i <= lensrc; i++)
+	{
+		dest[lendest + i] = src[i];
+	}
+	return (dest);
+}
