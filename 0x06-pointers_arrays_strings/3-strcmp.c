3a8525d07a138ba1daf59c0b8be0644e3af33cee
Ceery1 <mehdi.belmoumen@gmail.com>
2023-03-30 20:46:56 +0000

    comare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,20 +4,15 @@
  * _strcmp - function to call
  * @s1: first parameter
  * @s2: second parameter
- * Return: 0
+ * Return: z
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, z;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else
-			return (0);
-	}
-	return (0);
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+		i++;
+	z = s1[i] - s2[i];
+	return (z);
 }
