fd9d7f64af40b1d2c66c9519e3394dd4845ba4b6
Ceery1 <mehdi.belmoumen@gmail.com>
2023-04-03 20:09:22 +0000

    task 2 strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - function to call
+ * @s: string where to locate
+ * @c: character to locate
+ * Return: S
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i])
+			return (s + i);
+	}
+	return (s);
+}
