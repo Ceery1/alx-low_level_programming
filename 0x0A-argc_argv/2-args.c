6c4f03388cc8e15e657bb835820445a7aff047c3
Ceery1 <mehdi.belmoumen@gmail.com>
2023-04-09 22:13:45 +0000

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of argument in argv
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
