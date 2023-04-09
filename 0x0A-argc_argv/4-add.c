fc6c5b184d1935d029d44b8ee3dbbc3c50928c65
Ceery1 <mehdi.belmoumen@gmail.com>
2023-04-09 22:51:29 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+* @argc: number of arguments in argv
+* @argv: array of arguments
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int add = 0;
+	char k;
+
+	if (argc < 1)
+		printf("0");
+	for (i = 1; i < argc; i++)
+	{
+		k = *argv[i];
+		if (k < 48 || k > 57)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			add += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", add);	
+	return (0);
+}
