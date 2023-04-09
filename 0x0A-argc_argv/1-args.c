cdbb99ccca8a891d0b487ae766ab8dbb22ecbae4
Ceery1 <mehdi.belmoumen@gmail.com>
2023-04-09 22:05:01 +0000

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguments in argv
+ * @argv: array of arguments
+ * Return: 0 succes
+*/
+int main(int argc, char *argv[])
+{
+	int i, leng = 0;
+	(void) argv;
+
+
+	for (i = 1; i < argc; i++)
+	{
+		leng++;
+	}
+	printf("%d\n", leng);
+	return (0);
+}
