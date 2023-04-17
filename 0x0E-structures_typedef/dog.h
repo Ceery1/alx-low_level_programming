bff1e863d3d80f1ef9e5d10f3c27eb596fb6009b
Ceery1 <mehdi.belmoumen@gmail.com>
2023-04-17 20:34:43 +0000

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of dog's owner
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+#endif
