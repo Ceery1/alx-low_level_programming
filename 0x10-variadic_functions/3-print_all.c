a3eccb667bee71b6929866465bdf84e1b98f4472
Ceery1 <mehdi.belmoumen@gmail.com>
2023-04-20 22:12:03 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,12 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
-   * main - check the code
-    *
-     * Return: Always 0.
-      */
-int main(void)
+* print_all - function to print strings
+*
+* @format: number of parameter given
+* @...: format to print
+*/
+void print_all(const char * const format, ...)
 {
-	    print_all("ceis", 'B', 3, "stSchool");
-	        return (0);
+	int i;
+	char *s, *sep = "";
+
+	va_list args;
+
+	va_start(args, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args, double));
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s%s", sep, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(args);
 }
