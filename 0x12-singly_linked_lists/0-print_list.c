21ac42d8b9cb2b86957db53b0872a597c2478c22
Ceery1 <mehdi.belmoumen@gmail.com>
2023-04-27 18:38:42 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int count = 0;
+	size_t count = 0;
 
-	if (!h->str)
+	while (h)
+	{
+	if (h->str == NULL)
 		printf("[0] (nil)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
 	count++;
+	}
 	return (count);
 }
